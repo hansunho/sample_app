d09ef191ef9e176b378cb4db00c489bc75d29052
hansunho <hansunho@grinnell.edu>
2015-08-07 17:30:15 -0500

    Finish user eidt, update, index, and destroy actions

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ActiveRecord::Base
 
   before_save { self.email = email.downcase }
   validates :name, presence: true, length: {maximum: 50};
-  validates :password, presence: true, length: {minimum: 6}
+   validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 255 },
                     format: { with: VALID_EMAIL_REGEX },
